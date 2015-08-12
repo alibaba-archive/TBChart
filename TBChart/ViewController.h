d3adb8ef03e9397f1768ef064de8c340cc742ed6
HarriesChen <info@mrchenhao.com>
2015-08-12 13:59:58 +0800

    Initial Commit

---
+++ TBChart/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TBChart
+//
+//  Created by ChenHao on 8/12/15.
+//  Copyright (c) 2015 Teambition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TBChart/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TBChart
+//
+//  Created by ChenHao on 8/12/15.
+//  Copyright (c) 2015 Teambition. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
